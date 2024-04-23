7c489266ecca97329595b3c4f9875bbf5a93fac3
A o <aolin@ymail.com>
2024-04-23 23:43:43 +0300

    first commit|

---
+++ resources/views/about.blade.php
@@ -0,0 +1,8 @@
+<x-layout>
+    <x-slot:heading>
+        about page
+</x-slot:heading>
+    <h1 class="text-sky-400">About</h1>
+
+</x-layout>
+       
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,7 @@
+<x-layout>
+    <x-slot:heading>
+        contact us
+</x-slot:heading>
+    <h1 class="text-sky-400">Contact</h1>
+
+</x-layout>
